767b00c07388db0d344a3c5276606b3d731ca352
rabah-khalek <rabah.khalek@gmail.com>
2025-12-10 21:35:25 +0100

    fixed about title

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@
         const icon = document.createElement('span');
         icon.className = 'external-icon';
         icon.innerHTML = `
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle; margin-left: 4px;">
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 5L21 3" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         `;
